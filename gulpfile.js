13eb2951ea8d2eb6d2def62f01a33c2dfaf61188
moonhunter <vdcloayza@gmail.com>
2021-12-28 23:25:16 -0400

    sumando cambios
    sumando cambios

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+function defaultTask(cb) {
+    // place code for your default task here
+    cb();
+  }
+  
+  exports.default = defaultTask
+
+
+// var gulp=require('gulp')
+// var postcss=require('gulp-postcss')
+// var autoprefixer=require('autoprefixer')
+
+// gulp.task('styles',function(){
+//     return gulp.src('./*.css')
+//     .pipe(postcss([autoprefixer]))
+//     .pipe(gulp.dest('dest/'));
+// });
